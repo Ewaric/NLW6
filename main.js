7a0bc3010759e3735687d193498c5fa40c50ca31
Ewaric <ewaric.lininker@gmail.com>
2022-06-01 17:43:21 -0300

    Finalizando Menu e inserindo "sobre nós"

---
+++ main.js
@@ -0,0 +1,19 @@
+/* OPEN OR CLOSE MENU */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* WHEN CLICK MENU OPTIONS, HIDDEN MENU */
+
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
